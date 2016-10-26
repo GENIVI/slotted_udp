f0a9b3fead4c21fca3baa099197399373a2b1ffe
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2016-10-26 15:31:53 -0700

    Added master offset. Added raw packet transmission for special header slot 0 packets

--- slotted_udp.h
+++ slotted_udp.h
@@ -20,6 +20,7 @@ typedef struct _s_udp_channel_t {
 	struct sockaddr_in address; // Multicast address group.
 	uint32_t slot;    // Slot to use inside address:port
 	uint32_t slot_count;    // Number of slots maintained by this channel
+	uint32_t slot_width;    // The duration, in usec, of each slot.
 	int32_t socket_des;  // File descriptor
 	uint32_t max_latency;         // Max allowed latency, in microseconds
 	uint32_t min_latency;         // Min latency. Sleep prior to delivery if packet is received prior to this
@@ -30,6 +31,9 @@ typedef struct _s_udp_channel_t {
 	                              // Last received transaction ID for receiver.
 
 	uint8_t is_sender;            // Is this context single channel sender, or a receiver channel?
+	uint64_t master_clock_offset; // Microsecnds that self's clock is ahead of master clock.
+	                              // Master clock, sent out by slotted_udp_master program, will
+	                              // always have a lower value than the local clock.
 } s_udp_channel_t;
 
 
@@ -56,8 +60,15 @@ typedef enum _s_udp_err_t {
 						   ((uint64_t) tp.tv_nsec) / 1000LL)
 
 
-// Return microseconds since arbitrary start point.
+// Return synchronized microseconds since arbitrary start point
+// At any given time, this function will return the same
+// value for all processes connected to same address/port.
+//
+// Driven by slotted_udp_master program.
 extern uint64_t s_udp_get_master_clock(s_udp_channel_t* channel);
+
+// Return microseconds since arbitrary start point.
+extern uint64_t s_udp_get_monotonic_clock(void);
 
 extern const char* s_udp_error_string(s_udp_err_t code);
 
@@ -84,6 +95,14 @@ extern s_udp_err_t s_udp_get_socket_descriptor(s_udp_channel_t* channel,
 extern s_udp_err_t s_udp_send_packet(s_udp_channel_t* channel,
 									 const uint8_t* data,
 									 uint32_t length);
+
+extern s_udp_err_t s_udp_send_packet_raw(int socket_des,
+										 void* address,
+										 uint32_t slot,
+										 uint64_t transaction_id,
+										 uint64_t clock,
+										 const uint8_t* data,
+										 uint32_t length);
 
 extern s_udp_err_t s_udp_receive_packet(s_udp_channel_t* channel,
 										uint8_t* data,
